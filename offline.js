1c4fe327b6e8211d5f966d0e43e4c39e5c0d09e0
guruchian <30399464+semicon@users.noreply.github.com>
2019-09-17 22:58:04 +0700

    puzzle
    puzzle

---
+++ offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1568735642,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/image.png",
+		"images/grid-sheet0.png",
+		"images/frame-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
